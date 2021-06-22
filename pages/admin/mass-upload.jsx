2fb374eeec14dc6b59e7db441ca576b1a73c77f2
RuiFernandes <R.guirrugo@gmail.com>
2021-06-22 23:16:54 +0200

    Update mass-upload.jsx

--- pages/admin/mass-upload.jsx
+++ pages/admin/mass-upload.jsx
@@ -1,30 +1,41 @@
-import { Button, CircularProgress, Grid, Modal, TextField } from '@material-ui/core';
-import PageTemplate from '@components/admin/PageTemplate'
+import {
+	Button,
+	CircularProgress,
+	Grid,
+	Modal,
+	TextField,
+} from '@material-ui/core';
+import PageTemplate from '@components/admin/PageTemplate';
 
 const MassUpload = ({ useStore, firebase }) => {
-    const [loaded, setLoaded] = React.useState(false)
-    let state = useStore(state => state)
+	const [loaded, setLoaded] = React.useState(false);
+	let state = useStore((state) => state);
 
-    // Check if Page info is loaded
-    React.useEffect(() => { if (state.mapsLoaded) setLoaded(true) }, [state])
+	// Check if Page info is loaded
+	React.useEffect(() => {
+		if (state.mapsLoaded) setLoaded(true);
+	}, [state]);
 
-    return (
-        <PageTemplate head={"Settings"} pageName={"Settings"} useStore={useStore} firebase={firebase}>
-            {!loaded && <CircularProgress color="secondary" />}
-            <section style={{ display: loaded ? 'block' : 'none' }}>
-                <Grid container spacing={3}>
-                    <div>
-                        <h1>Upload JSON</h1>
-                        <input type="file"></input>
+	return (
+		<PageTemplate
+			head={'Settings'}
+			pageName={'Settings'}
+			useStore={useStore}
+			firebase={firebase}>
+			{!loaded && <CircularProgress color='secondary' />}
+			<section style={{ display: loaded ? 'block' : 'none' }}>
+				<Grid container spacing={3}>
+					<div>
+						<h1>Upload JSON</h1>
+						<input type='file' onChange={uploadJSON}></input>
 
-                        <h1 style={{ marginTop: '1em' }}>Upload PDFS</h1>
-                        <input type="file" multiple></input>
+						<h1 style={{ marginTop: '1em' }}>Upload PDFS</h1>
+						<input type='file' multiple></input>
+					</div>
+				</Grid>
+			</section>
+		</PageTemplate>
+	);
+};
 
-                    </div>
-
-                </Grid>
-            </section>
-        </PageTemplate >)
-}
-
-export default MassUpload
+export default MassUpload;
