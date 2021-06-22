237c0dfeab4ed0676a736570f04c4ff377e9bd85
RuiFernandes <R.guirrugo@gmail.com>
2021-06-22 23:54:47 +0200

    Update mass-upload.jsx

--- pages/admin/mass-upload.jsx
+++ pages/admin/mass-upload.jsx
@@ -10,6 +10,35 @@ import PageTemplate from '@components/admin/PageTemplate';
 const MassUpload = ({ useStore, firebase }) => {
 	const [loaded, setLoaded] = React.useState(false);
 	let state = useStore((state) => state);
+	const uploadJSON = (e) => {
+		// const json = require(e.target.value);
+		// console.log(e.target.files[0]);
+		// var reader = new FileReader();
+		// reader.readAsText(JSON.parse(e.target.files[0]));
+		// var fs = require("fs");
+		let db = firebase.firestore();
+		const reader = new FileReader();
+		reader.onload = function () {
+			const json = JSON.parse(reader.result).data;
+			json.forEach((isapre) => {
+				db.collection('insurance_entries')
+					.add(isapre)
+					.then((docRef) => {
+						console.log('Document written with ID: ', docRef.id);
+					})
+					.catch((error) => {
+						console.error('Error adding document: ', error);
+					});
+			});
+
+			//   console.log(JSON.parse(reader.result).data);
+		};
+		reader.readAsText(e.target.files[0]);
+		console.log(e.target.files[0]);
+
+		// let db = firebase.firestore();
+		// db.collection('insurance_entries')
+	};
 
 	// Check if Page info is loaded
 	React.useEffect(() => {
